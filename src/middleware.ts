ac6862ef44b7efc061b5b0aa968d187caabd5280
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-09-22 02:43:35 +0530

    Final Commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,11 +19,11 @@ export async function middleware(request: NextRequest) {
         (url.pathname.startsWith('/verify')) ||
         (url.pathname.startsWith('/'))
     ){
-        // return NextResponse.redirect(new URL('/dashboard', request.url))
+        return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    // if(!token && url.pathname.startsWith('/dashboard')){
-    //     return NextResponse.redirect(new URL ('/signin', request.url))
-    // }
+    if(!token && url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL ('/signin', request.url))
+    }
 
   return NextResponse.next()
 }
